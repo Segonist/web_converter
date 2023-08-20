92735e2110171e0b0e216cb48fd5475ee57f001c
Segonist <andrey07.nep@gmail.com>
2023-08-20 12:19:08 +0200

    Add: added and configured vitest library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: {
+		globals: true,
+		coverage: {
+			provider: "istanbul",
+			reportsDirectory: "./src/__tests__/coverage",
+		},
+	},
+});
